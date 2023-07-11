18c4cda86258b206f32ff38c48ebcb0dddfe0bf1
newsbubbles <nathaniel.gibson@gmail.com>
2023-07-11 10:16:35 -0500

    Added autocomplete and Prompt history; tab and arrow keys

--- bashir.py
+++ bashir.py
@@ -5,6 +5,11 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage, AIMessage
 import openai
 import getpass
+try:
+    import readline
+except ImportError:
+    import pyreadline as readline
+
 
 ## Support
 def parse_args():
@@ -31,20 +36,27 @@ def save_script(content, comment=None):
 # Context Settings: can be any OS and command line interpreter
 operating_system, cli = parse_args()
 
-# Confirmation state
+# Confirmation state and other vars
 CONFIRM_NONE, CONFIRM_ALL, CONFIRM_SUDO = 0, 1, 2
 confirmation = CONFIRM_NONE
+
+# Configure prompt history for fancier more util input
+try:
+    readline.read_history_file()
+except FileNotFoundError:
+    pass
 
 # Config langchain chat model
 openai.api_key = os.getenv("OPENAI_API_KEY")
 chat = ChatOpenAI(model_name="gpt-3.5-turbo",temperature=1.24)
-system_message = SystemMessage(content=f"You are an advanced coding translator. You take natural language and convert whatever the user explains they want to do as a sequence of {cli} commands for {operating_system}. If a user asks how much space they have left on their hard drive, you output the proper commands needed to run in order to see that as a final output. Do not include explanations, only the translation into bash. Always use command arguments that bypass user confirmation like -y when using `apt install`.")
+system_message = SystemMessage(content=f"You are an advanced coding translator. You take natural language and convert whatever the user explains they want to do as a sequence of {cli} commands for {operating_system}. If a user asks how much space they have left on their hard drive, you output the proper commands needed to run in order to see that as a final output. Do not include explanations, only the translation into {cli}. Always use command arguments that bypass user confirmation like -y when using `apt install`.")
 
 # Prepare scripts folder
 sdir = os.path.join(os.getcwd(), 'scripts')
 if not os.path.exists(sdir):
     os.makedirs(sdir)
 
+# Title
 print('\n--------------------------------------------\nBashir: LLM based Junior System Admin v0.1.0\n--------------------------------------------\n')
 print('OS:', operating_system, '\nCLI:', cli, '\n')
 
@@ -70,7 +82,10 @@ while True:
         # If the user types 'exit', then end the loop and close
         if command.strip() == 'exit':
             break
-        
+
+        # Write history file for pretty history
+        readline.write_history_file()
+
         # Perform call to LLM and get a bash script in return
         response = chat([system_message, HumanMessage(content=command)])
         bash_script = response.content
@@ -83,15 +98,22 @@ while True:
         time.sleep(0.5)
         
         # Expect the command prompt again or asking for sudo pass
-        eindex = child.expect([def_prompt, sudo_prompt])
-        if eindex == 1 and password is not None:
-            child.sendline(password)
-            child.expect(def_prompt)
+        try:
+            eindex = child.expect([def_prompt, sudo_prompt], timeout=10)
+            if eindex == 1 and password is not None:
+                child.sendline(password)
+                child.expect(def_prompt)
+        except pexpect.TIMEOUT:
+            print(child.before.decode('utf-8').strip())
+            # if child.isalive():
+            #     child.sendintr()
+            continue
+        except pexpect.EOF:
+            print(child.before.decode('utf-8').strip())
+            break
         
         # Print the output of the command
         print(child.before.decode('utf-8').strip())
-    except pexpect.EOF:
-        break
     except Exception as e:
         print(str(e))
 
